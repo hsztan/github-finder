6292c98904d987b72e7ebbace0b8573d62aa4fec
Henry Nawrocki <hdev@sztanski.com>
2022-06-03 14:59:54 -0500

    Implement the search users fetch to github api and display them using Context Api

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -4,7 +4,7 @@ import GithubContext from '../../context/github/GithubContext';
 function UserSearch() {
   const [text, setText] = useState('');
 
-  const { users } = useContext(GithubContext);
+  const { users, searchUsers } = useContext(GithubContext);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -14,7 +14,7 @@ function UserSearch() {
     e.preventDefault();
     if (text === '') alert('Please enter a username');
     else {
-      //todo serach users
+      searchUsers(text);
       setText('');
     }
   };
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,26 +14,35 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const fetchUsers = async () => {
-    const res = await fetch(`${GITHUB_URL}users`, {
+  // Get search results
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const res = await fetch(`${GITHUB_URL}search/users?${params}`, {
       method: 'GET',
       // headers: {
       //   Authorization: `token ${GITHUB_TOKEN} `,
       // },
     });
-    const data = await res.json();
+    const { items } = await res.json();
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     });
   };
+
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
       }}
     >
       {children}

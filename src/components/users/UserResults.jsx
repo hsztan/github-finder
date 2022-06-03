97f52b6a70ccdf662a9a73dc07d08dd563889fa6
Henry Nawrocki <hdev@sztanski.com>
2022-06-03 12:35:07 -0500

    Add context with reducers

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,18 +1,19 @@
-// import { useEffect } from 'react';
-import useSWR from 'swr';
+import { useContext, useEffect } from 'react';
 import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
 
 function UserResults() {
-  const { data, error } = useSWR(`${process.env.REACT_APP_GITHUB_URL}users`, {
-    headers: {
-      Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    },
-  });
-  console.log(data);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  console.log(users);
 
   return (
     <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {data.map((user, i) => (
+      {users.map((user, i) => (
         <UserItem key={user.id} user={user} />
       ))}
     </div>
